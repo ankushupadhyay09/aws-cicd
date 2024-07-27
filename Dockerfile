53f7a54102bf357660ef10982a4cdd8ae6025fc0
Ankush Upadhyay <ankush.upadhyay@hotwaxsystems.com>
2024-07-27 21:29:10 +0530

    Adding course-api

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk:17
+
+WORKDIR /app
+
+COPY ./target/course-api.jar /app
+
+EXPOSE 8080
+
+CMD ["java", "-jar", "course-api.jar"]
---
+++ src/main/java/com/restapi/course_api/controller/CourseController.java
@@ -0,0 +1,30 @@
+package com.restapi.course_api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+record Course(String name, String category) {
+
+}
+@RestController
+public class CourseController {
+    public static List<Course> courseList = new ArrayList<>();
+
+    @GetMapping("/courses")
+    public List<Course> getAllCourses() {
+        return courseList;
+    }
+
+    @PostMapping("/courses")
+    public ResponseEntity addCourse(@RequestBody Course course) {
+        courseList.add(course);
+        return ResponseEntity.ok().build();
+    }
+
+}
